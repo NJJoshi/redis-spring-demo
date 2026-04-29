facbf1fc10eb4f1802843ac7d843dd0318cd1fec
Nirav Joshi <joshi.nirav21@gmail.com>
2026-04-29 17:12:22 +0530

    Initial commit

---
+++ src/main/java/com/nj/play/redis/redisson/redisspringdemo/RedisSpringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.nj.play.redis.redisson.redisspringdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSpringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisSpringDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nj/play/redis/redisson/redisspringdemo/RedisSpringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nj.play.redis.redisson.redisspringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
