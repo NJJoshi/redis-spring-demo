d2b50d61b03b807aaf43755ed82016572555d72d
Nirav Joshi <joshi.nirav21@gmail.com>
2026-04-29 19:30:27 +0530

    Changes related to demonstrate cache evict strategy with redis using redisson library

--- src/main/java/com/nj/play/redis/redisson/redisspringdemo/fib/service/FibService.java
+++ src/main/java/com/nj/play/redis/redisson/redisspringdemo/fib/service/FibService.java
@@ -1,15 +1,24 @@
 package com.nj.play.redis.redisson.redisspringdemo.fib.service;
 
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FibService {
 
+    //Need to have a cache evict strategy
     @Cacheable(value = "math:fib", key = "#n")
     public int getFib(int n, String name) {
         System.out.println("Calculating fib for " + n + " and name " + name);
         return fib(n);
+    }
+
+    // PUT/ POST / POST/ PATCH / DELETE
+    @CacheEvict(value = "math:fib", key = "#n")
+    public void clearCache(int n) {
+        System.out.println("Clearing cache for key =" + n);
+
     }
 
     private int fib(int n) {
