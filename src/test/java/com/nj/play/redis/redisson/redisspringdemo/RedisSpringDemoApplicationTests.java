ddcb650e43893d79259ea603babbbe47cd998d65
Nirav Joshi <joshi.nirav21@gmail.com>
2026-04-29 18:39:27 +0530

    Added new test case for redisson performance test

--- src/test/java/com/nj/play/redis/redisson/redisspringdemo/RedisSpringDemoApplicationTests.java
+++ src/test/java/com/nj/play/redis/redisson/redisspringdemo/RedisSpringDemoApplicationTests.java
@@ -2,6 +2,8 @@ package com.nj.play.redis.redisson.redisspringdemo;
 
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.redisson.api.RAtomicLongReactive;
+import org.redisson.api.RedissonReactiveClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.ReactiveStringRedisTemplate;
@@ -16,12 +18,27 @@ class RedisSpringDemoApplicationTests {
     @Autowired
     private ReactiveStringRedisTemplate template;
 
+    @Autowired
+    private RedissonReactiveClient client;
+
     @RepeatedTest(3)
     void springDataRedisTest() {
         ReactiveValueOperations<String, String> valueOperation = this.template.opsForValue();
         long start = System.currentTimeMillis();
         Mono<Void> mono = Flux.range(1, 500_000)
                 .flatMap(i -> valueOperation.increment("usr:1:visit", i))
+                .then();
+        StepVerifier.create(mono).verifyComplete();
+        long end = System.currentTimeMillis();
+        System.out.println((end - start) + " ms");
+    }
+
+    @RepeatedTest(3)
+    void springDataRedissonTest() {
+        RAtomicLongReactive atomicLong = this.client.getAtomicLong("user:2:visit");
+        long start = System.currentTimeMillis();
+        Mono<Void> mono = Flux.range(1, 500_000)
+                .flatMap(i -> atomicLong.incrementAndGet())
                 .then();
         StepVerifier.create(mono).verifyComplete();
         long end = System.currentTimeMillis();
